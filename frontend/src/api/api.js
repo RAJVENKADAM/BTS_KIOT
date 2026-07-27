4865c8fffd57adb5a636cd8608b43c431569218d
Rajvenkadam <amux525@gmail.com>
2026-07-27 22:36:09 +0530

    after rendering

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -4,8 +4,8 @@
  * API configuration and health check utility.
  * Defines the base URL for all backend API calls.
  */
-//export const API_BASE_URL = "https://bts-bus-tracking-system-2.onrender.com";
-export const API_BASE_URL = "http://10.131.193.77:5000";
+export const API_BASE_URL = "https://bts-kiot.onrender.com";
+//export const API_BASE_URL = "http://10.131.193.77:5000";
 
 export const healthCheck = async () => {
   try {
